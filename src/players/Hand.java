6b192e3a65114f08c7d1d1e0ed1bac5faa285e71
Jelle Nijland <jelle.nijland@xmsnet.nl>
2016-11-17 12:04:35 +0100

    Finished hand.java

---
+++ src/players/Hand.java
@@ -0,0 +1,59 @@
+package players;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gamefiles.Card;
+
+public class Hand {
+	private List<Card> hand;
+	
+	public Hand(){
+		hand = new ArrayList<Card>();
+	}
+	
+	public void clear(){
+		hand.clear();
+	}
+	
+	public void addCard(Card c){
+		hand.add(c);
+	}
+	
+	public void removeCard(Card c){
+		hand.remove(c);
+	}
+	
+	public void removeCard(int pos){
+		hand.remove(pos);
+	}
+	
+	public int getCardCount(){
+		return hand.size();
+	}
+	
+	public Card getCard(int pos){
+		if(hand.get(pos)==null){
+			return null;
+		}
+		else{
+			return hand.get(pos);
+		}
+	}
+	
+	public List<Card> handCopy(){
+		List<Card> copy = new ArrayList<Card>(hand);
+		return copy;
+	}
+	
+	public String toString(){
+		return hand.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
