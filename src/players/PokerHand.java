86ce8dce6561d57b25e4b8aef233d79a3a2417dc
Jelle Nijland <jelle.nijland@xmsnet.nl>
2016-11-17 12:13:31 +0100

    Finished boilerplate code for PokerHand

---
+++ src/players/PokerHand.java
@@ -0,0 +1,20 @@
+package players;
+
+import java.util.ArrayList;
+
+import gamefiles.Card;
+
+//TODO: remove warning suppressor
+@SuppressWarnings("unused")
+public class PokerHand extends Hand{
+	
+	
+	private ArrayList<ArrayList<Card>> res;
+	
+	public ArrayList<ArrayList<Card>> getHandCombinations(){
+		ArrayList<ArrayList<Card>> res = new ArrayList<ArrayList<Card>>();
+		//TODO: write implementation
+		return res;
+	}
+
+}
