e05998f5f02716191573484ea76c006dd8817651
KingArthur <r.k.deridder@student.utwente.nl>
2016-11-23 15:10:40 +0100

    ...

--- src/gamefiles/Poker.java
+++ src/gamefiles/Poker.java
@@ -1,13 +1,5 @@
 package gamefiles;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import players.User;
-import util.HandTuple;
-
-// Shouldn't this be in player since it's player specific?
-
 public class Poker {
 
 }
