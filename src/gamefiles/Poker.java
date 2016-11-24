4bf1c0a8d344036d17fd1d03a1457a806e5621a9
Jelle Nijland <jelle.nijland@xmsnet.nl>
2016-11-24 11:56:18 +0100

    Commit message

--- src/gamefiles/Poker.java
+++ src/gamefiles/Poker.java
@@ -38,15 +38,24 @@ public class Poker {
 	}
 
 	public User tieResolution(HashMap<User, HandTuple<ArrayList<Card>, Integer>> hands){
-		ArrayList<User> users = new ArrayList<User>(hands.keySet());
+		//Split the hashmap so we can work with it
+		// We'll compare the hands and then ask which key fits the hand (find user by hand)
+		ArrayList<User> usersTie = new ArrayList<User>(hands.keySet());
 		ArrayList<HandTuple<ArrayList<Card>, Integer>> hand = new ArrayList<HandTuple<ArrayList<Card>, Integer>>(hands.values());
+		int highCardVal;
 		if(hands.keySet().size() == 1){
-			return users.get(0);
+			return usersTie.get(0);
 		}
 		int rank = hands.get(0).showRank();
 		switch(rank){
 			case(1):
+				highCardVal = hand.get(0).showSet().get(4).getValue().getValueInt();
+				for(int i = 1; i < hand.size() - 1; i++){
+					if(hand.get(i-1).showSet().get(4).getValue().getValueInt() < hand.get(i).showSet().get(4).getValue().getValueInt()){
+						highCardVal = hand.get(i).showSet().get(4).getValue().getValueInt();
+					}
 				
+				}
 			case(2):
 				
 			case(3):
